70b97ec8d490ffda286a52446b66ee844efad337
Kaare <kaare@PurpleLakeDog.local>
2018-05-30 13:00:51 +0200

    Improved visuals

--- application/views/posts/post.php
+++ application/views/posts/post.php
@@ -1,10 +1,14 @@
-<div class="row border bg-light mt-3">
-    <div class="col-md-2 border">
-        <img src="http://www.teatro.it/old/2016-11/nobody_m.original.jpg" alt="..." class="img-thumbnail m-1">
-    </div>
-    <div class="col-md-10 border">
-        <p><? echo anchor("user/$owner->id", "$owner->firstname $owner->lastname")?></p>
-        <p><?php echo $body?></p>
-        <p class="meta"><?php echo $timestamp?></p>
+<div class="card mt-3">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-2">
+                <img src="http://www.teatro.it/old/2016-11/nobody_m.original.jpg" alt="..." class="img-thumbnail">
+            </div>
+            <div class="col-md-10">
+                <h5 class="card-title"><? echo anchor("user/$owner->id", "$owner->firstname $owner->lastname")?></h5>
+                <p class="meta"><?php echo $timestamp?></p>
+            </div>
+        </div>
+        <p class="card-text"><?php echo $body?></p>
     </div>
 </div>
